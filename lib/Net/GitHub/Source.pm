6cbc313c0af11ef73245e0569801c4151063cc5c
unknown <Administrator@.(none)>
2009-03-07 11:51:03 +0800

    init Net::GitHub::Source

---
+++ lib/Net/GitHub/Source.pm
@@ -0,0 +1,17 @@
+package Net::GitHub::Source;
+
+use Moose;
+
+our $VERSION = '0.01';
+our $AUTHORITY = 'cpan:FAYLAND';
+
+has 'username' => ( isa => 'Str', is => 'ro', required => 1 );
+has 'password' => ( isa => 'Str', is => 'rw' );
+has 'project'  => ( isa => 'Str', is => 'ro', required => 1 );
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__
