16582dc65d0d1b80ec8839558d61d4ba7baa5021
unknown <Administrator@.(none)>
2009-03-07 11:47:50 +0800

    start to work

--- lib/Net/GitHub.pm
+++ lib/Net/GitHub.pm
@@ -1,98 +1,37 @@
 package Net::GitHub;
 
-use warnings;
-use strict;
+use Moose;
+
+our $VERSION = '0.01';
+our $AUTHORITY = 'cpan:FAYLAND';
+
+has 'username' => ( isa => 'Str', is => 'ro', required => 1 );
+has 'password' => ( isa => 'Str', is => 'rw' );
+has 'project'  => ( isa => 'Str', is => 'ro', required => 1 );
+
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__
 
 =head1 NAME
 
-Net::GitHub - The great new Net::GitHub!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
+Net::GitHub - Perl Interface for github.com
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
     use Net::GitHub;
 
-    my $foo = Net::GitHub->new();
+    my $foo = Net::GitHub->new( username => 'fayland', project => 'perl-net-github' );
     ...
 
 =head1 EXPORT
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
-
 =head1 AUTHOR
 
 Fayland Lam, C<< <fayland at gmail.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-net-github at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-GitHub>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Net::GitHub
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Net-GitHub>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Net-GitHub>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Net-GitHub>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Net-GitHub/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
 
 =head1 COPYRIGHT & LICENSE
 
@@ -100,8 +39,3 @@ Copyright 2009 Fayland Lam, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
-
-
-=cut
-
-1; # End of Net::GitHub
